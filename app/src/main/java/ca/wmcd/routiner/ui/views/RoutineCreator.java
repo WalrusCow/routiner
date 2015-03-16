fa25fba5bc805d390f5fd43c5dc90585ff437ea4
WalrusCow <willgmcd@gmail.com>
2015-03-15 21:33:11 -0700

    Circle select weekdays

---
+++ app/src/main/java/ca/wmcd/routiner/ui/views/RoutineCreator.java
@@ -0,0 +1,47 @@
+package ca.wmcd.routiner.ui.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectViews;
+import ca.wmcd.routiner.R;
+
+/**
+ * Created by WalrusCow on 3/15/15.
+ */
+public class RoutineCreator extends LinearLayout {
+    @InjectViews(
+            {R.id.weekday_sunday, R.id.weekday_monday, R.id.weekday_tuesday, R.id.weekday_wednesday,
+             R.id.weekday_thursday, R.id.weekday_friday, R.id.weekday_saturday})
+    List<View> weekdayViews;
+
+    public RoutineCreator(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public RoutineCreator(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public RoutineCreator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public RoutineCreator(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context) {
+        inflate(context, R.layout.view_routine_creator, this);
+        ButterKnife.inject(this, this);
+    }
+}
--- app/src/main/java/ca/wmcd/routiner/ui/views/WeekdaySelector.java
+++ app/src/main/java/ca/wmcd/routiner/ui/views/WeekdaySelector.java
@@ -1,38 +1,51 @@
 package ca.wmcd.routiner.ui.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import ca.wmcd.routiner.R;
 
 /**
  * Created by WalrusCow on 3/15/15.
  */
-public class WeekdaySelector extends Button {
+public class WeekdaySelector extends TextView {
+    private static String shortText[] = {"S", "M", "T", "W", "T", "F", "S"};
     public WeekdaySelector(Context context) {
         super(context);
-        init();
+        init(context, null);
     }
 
     public WeekdaySelector(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context, attrs);
     }
 
     public WeekdaySelector(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context, attrs);
     }
 
     public WeekdaySelector(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init();
+        init(context, attrs);
     }
 
-    private void init() {
+    private void init(Context context, AttributeSet attrs) {
         ButterKnife.inject(this, this);
+        // Extract custom attribute
+        if (attrs == null) {
+            return;
+        }
+
+        TypedArray typedArray =
+                context.obtainStyledAttributes(attrs, R.styleable.WeekdaySelector, 0, 0);
+        int weekDay = typedArray.getInt(R.styleable.WeekdaySelector_weekday, 0);
+        setText(shortText[weekDay]);
+        typedArray.recycle();
     }
 
     @OnClick
