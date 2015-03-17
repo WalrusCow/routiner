72ad761313c4c49f360cc47ee8f640e86edf6d79
WalrusCow <willgmcd@gmail.com>
2015-03-16 22:51:11 -0700

    Wake service in future and notify

---
+++ app/src/main/java/ca/wmcd/routiner/RoutineNotifier.java
@@ -0,0 +1,41 @@
+package ca.wmcd.routiner;
+
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import timber.log.Timber;
+
+/**
+ * Created by WalrusCow on 3/15/15.
+ * Service to send notifications when a routine is due.
+ */
+public class RoutineNotifier extends Service {
+    public RoutineNotifier() {
+        super();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handleStart(intent);
+        return START_FLAG_RETRY;
+    }
+
+    private void handleStart(Intent intent) {
+        // TODO: Notify or something
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("Routine Notifier");
+        builder.setContentText("This is the details");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(1, builder.build());
+        Timber.d("Hello");
+    }
+}
--- app/src/main/java/ca/wmcd/routiner/ui/views/RoutineCreator.java
+++ app/src/main/java/ca/wmcd/routiner/ui/views/RoutineCreator.java
@@ -1,19 +1,25 @@
 package ca.wmcd.routiner.ui.views;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import java.util.Calendar;
 import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectViews;
 import butterknife.OnClick;
 import ca.wmcd.routiner.R;
+import ca.wmcd.routiner.RoutineNotifier;
 
 /**
  * Created by WalrusCow on 3/15/15.
+ * View to create a new routine
  */
 public class RoutineCreator extends LinearLayout {
     @InjectViews(
@@ -51,5 +57,17 @@ public class RoutineCreator extends LinearLayout {
         for (View v : weekdayViews) {
             v.setEnabled(!v.isEnabled());
         }
+    }
+
+    @OnClick(R.id.clicky)
+    public void clickyClick() {
+        Context context = getContext();
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent intent = PendingIntent.getService(
+                context, 0, new Intent(context, RoutineNotifier.class), 0);
+
+        // Five seconds from now
+        long wakeTime = Calendar.getInstance().getTimeInMillis() + 5000;
+        am.set(AlarmManager.RTC_WAKEUP, wakeTime, intent);
     }
 }
