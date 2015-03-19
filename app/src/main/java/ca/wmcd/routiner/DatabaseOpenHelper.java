c70ebcd781bd51388af8e523ce7317b7a6559e90
WalrusCow <willgmcd@gmail.com>
2015-03-18 22:06:03 -0700

    "Final" schema

--- app/src/main/java/ca/wmcd/routiner/DatabaseOpenHelper.java
+++ app/src/main/java/ca/wmcd/routiner/DatabaseOpenHelper.java
@@ -12,14 +12,16 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     private static final String KEY_GOAL = "goal";
-    private static final String KEY_DAYS = "days";
+    private static final String KEY_WEEKDAYS = "weekdays";
+    private static final String KEY_DAY_INTERVAL = "day_interval";
     private static final String KEY_TIME = "time";
 
     private static final String ROUTINES_TABLE_NAME = "routines";
     private static final String ROUTINES_TABLE_CREATE =
             "CREATE TABLE IF NOT EXISTS " + ROUTINES_TABLE_NAME + "(" +
             KEY_GOAL + " TEXT, " +
-            KEY_DAYS + " INTEGER, " +
+            KEY_WEEKDAYS + " INTEGER, " +
+            KEY_DAY_INTERVAL + " INTEGER, " +
             KEY_TIME + " INTEGER);";
 
 
